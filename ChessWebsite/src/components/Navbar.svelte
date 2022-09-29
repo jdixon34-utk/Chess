a677b5fe12f13c21ab2bcf2e74611584180cec69
Jake <JakeShoffner27@gmail.com>
2022-09-29 08:32:39 -0400

    navbar component creation

---
+++ ChessWebsite/src/components/Navbar.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<div class="w-full h-[8vh] bg-[#171717] text-white flex flex-row justify-between items-center">
+    <h1 class="ml-6">Chess Website&trade</h1>
+    <div class="flex flex-row items-center">
+        <h1 class="mr-6">Play</h1>
+        <h1 class="mr-6">How To Play</h1>
+        <h1 class="mr-6">About Project</h1>
+        <h1 class="mr-6">Github</h1>
+    </div>
+</div>
