5d450ad554bfb21ad753efe1cdce5a1bb1a6aa4d
Jake <JakeShoffner27@gmail.com>
2022-10-04 09:12:16 -0400

    Added website routing
    - added the routes "/learn" & "/about"

--- ChessWebsite/src/components/Navbar.svelte
+++ ChessWebsite/src/components/Navbar.svelte
@@ -1,13 +1,27 @@
 <script>
+    import Icon from '@iconify/svelte';
 
+    /* Variables */
+    let noMenu = true;
+
+    /* Handlers and other functions */
+    const CloseMenu = () => noMenu = true;
+    const OpenMenu = () => noMenu = false;
 </script>
 
-<div class="w-full h-[8vh] bg-[#171717] text-white flex flex-row justify-between items-center">
-    <h1 class="ml-6">Chess Website&trade</h1>
-    <div class="flex flex-row items-center">
-        <h1 class="mr-6">Play</h1>
-        <h1 class="mr-6">How To Play</h1>
-        <h1 class="mr-6">About Project</h1>
-        <h1 class="mr-6">Github</h1>
-    </div>
+<!-- This is the top of the Navigation Bar -->
+<div class="w-full h-[9vh] bg-[#171717] text-white flex flex-row justify-between items-center text-[25px] fixed z-40">
+    <a href="/" class="ml-6"><h1>ChadChess&trade</h1></a>
+
+    <!-- This is only viewed on mobile, right side of Navbar -->
+    {#if noMenu === true}
+        <div class="text-4xl md:hidden mr-6" on:click={() => OpenMenu()}>
+            <Icon icon="line-md:close-to-menu-alt-transition" />
+        </div>
+    {:else if noMenu === false}
+        <div class="text-4xl md:hidden mr-6" on:click={() => CloseMenu()}>
+            <Icon icon="line-md:menu-to-close-alt-transition" />
+        </div>
+    {/if}
 </div>
+
