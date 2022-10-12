46848192a5c8c0613451d6117ab6a58f10002d7f
jdixon34-cs340 <112104045+jdixon34-cs340@users.noreply.github.com>
2022-10-12 14:51:07 -0400

    updated genMoves() and made lookup tbl functions global

--- src/board.cpp
+++ src/board.cpp
@@ -170,20 +170,21 @@ int Board::getLSBIndex(unsigned long long bitBoard){
 
 	return __builtin_popcountll(bitBoard) - 1;
 }
+
 int Board::getMSBIndex(unsigned long long bitBoard){
 	return 63 - __builtin_clzll(bitBoard);
 }
 
 void Board::genMoves(){
-	/*
 	unsigned long long tmpBitBoard;
 	int square;
 
 	if(whiteTurn){
-
+		/*
 		instead of looping through the whole bitboard to find bits set to one,
 		we can keep finding the lsb set to one, do what we need to do with it,
 		set it to zero, and repeat until the bitboard == 0
+		*/
 
 		tmpBitBoard = whiteKing;
 		while(whiteKing){
@@ -193,22 +194,43 @@ void Board::genMoves(){
 		}
 		whiteKing = tmpBitBoard;
 
-		tmpBitBoard = whiteKnight;
-		while(whiteKnight){
-			square = getLSBIndex(whiteKnight);
-			genKnightMoves(square);
-			whiteKnight ^= 1ULL << square;
+		tmpBitBoard = whiteQueen;
+		while(whiteQueen){
+			square = getLSBIndex(whiteQueen);
+			genQueenMoves(square);
+			whiteQueen ^= 1ULL << square;
 		}
-		whiteKnight = tmpBitBoard;
+		whiteQueen = tmpBitBoard;
 
-		just do king and knight for now because they are easy
+		tmpBitBoard = whiteRooks;
+		while(whiteRooks){
+			square = getLSBIndex(whiteRooks);
+			genRookMoves(square);
+			whiteRooks ^= 1ULL << square;
+		}
+		whiteRooks = tmpBitBoard;
+
+		tmpBitBoard = whiteBishops;
+		while(whiteBishops){
+			square = getLSBIndex(whiteBishops);
+			genBishopMoves(square);
+			whiteBishops ^= 1ULL << square;
+		}
+		whiteBishops = tmpBitBoard;
+
+		tmpBitBoard = whiteKnights;
+		while(whiteKnights){
+			square = getLSBIndex(whiteKnights);
+			genKnightMoves(square);
+			whiteKnights ^= 1ULL << square;
+		}
+		whiteKnights = tmpBitBoard;
+
 	}
 	else{
 
-		same thing but for black
 	}
 
-	*/
 }
 
 void Board::genKingMoves(int square){
@@ -337,7 +359,6 @@ void Board::genQueenMoves(int square){
 			queen_moves &= ~RAYS[new_square][7];
 		}
 	}
-	printBitBoard(queen_moves);
 }
 
 void Board::genRookMoves(int square){
@@ -438,10 +459,10 @@ void Board::genBishopMoves(int square){
 			bishop_moves &= ~RAYS[new_square][3];
 		}
 		//sw
-//		bishop_moves |= (RAYS[square][5] ^ (blackPieces & RAYS[square][5]));
+		bishop_moves |= (RAYS[square][5] ^ (blackPieces & RAYS[square][5]));
 		if(RAYS[square][5] & allPieces){
 			new_square = getMSBIndex(RAYS[square][5]&allPieces);
-//			bishop_moves &= ~RAYS[new_square][5];
+			bishop_moves &= ~RAYS[new_square][5];
 		}
 		//nw
 		bishop_moves |= (RAYS[square][7] ^ (blackPieces & RAYS[square][7]));
@@ -453,15 +474,12 @@ void Board::genBishopMoves(int square){
 }
 
 void Board::genKnightMoves(int square){
-        unsigned long long knightMoves = 0;
+    unsigned long long knightMoves = 0;
     if(whiteTurn){
         knightMoves = KNIGHT_LOOKUP_TBL[square] ^ (whitePieces & KNIGHT_LOOKUP_TBL[square]);
     }else{ // Black's Turn
         knightMoves = KNIGHT_LOOKUP_TBL[square] ^ (blackPieces & KNIGHT_LOOKUP_TBL[square]);
     }
-
-    printf("%llu\n", knightMoves);
-    printBitBoard(knightMoves);
 
     int index;
     while(knightMoves){
@@ -503,144 +521,4 @@ void Board::genCastleKS(){
 
 void Board::genCastleQS(){
 
-}
-
-void Board::INITIALIZE_KING_LOOKUP_TBL(){
-	for(unsigned long long i=0; i<64; i++){
-		//Kings move to the right
-		if((i+1) % 8){
-			KING_LOOKUP_TBL[i] |= 1ULL << (i+1);
-			// Up to the Right
-			if(i+1 < 57) KING_LOOKUP_TBL[i] |= 1ULL << (i+9);
-			//Down to the Right
-			if(i+1 > 7) KING_LOOKUP_TBL[i] |= 1ULL << (i-7);
-		}
-
-		//Kings move to the left
-		if((i-1) % 8 != 7){
-			KING_LOOKUP_TBL[i] |= 1ULL << (i-1);
-			// Left-Up
-			if(i-1 < 57) KING_LOOKUP_TBL[i] |= 1ULL << (i+7);
-			// Left-Down
-			if(i-1 > 7) KING_LOOKUP_TBL[i] |= 1ULL << (i-9);
-		}
-
-		//King moves up
-		if(i < 57) KING_LOOKUP_TBL[i] |= 1ULL << (i+8);
-
-		//King moves down
-		if(i > 7) KING_LOOKUP_TBL[i] |= 1ULL << (i-8);
-	}
-}
-
-void Board::INITIALIZE_KNIGHT_LOOKUP_TBL(){
-	unsigned long long tmpBitBoard;
-	int rNew;
-	int cNew;
-
-	for(int c = 0; c < 8; c++){
-		for(int r = 0; r < 8; r++){
-			tmpBitBoard = 0;
-			rNew = r + 1;
-			cNew = c + 2;
-			if (rNew < 8 && cNew < 8){
-				tmpBitBoard |= (1ULL << ((cNew * 8) + rNew));
-			}
-			rNew = r + 2;
-			cNew = c + 1;
-			if (rNew < 8 && cNew < 8){
-				tmpBitBoard |= (1ULL << ((cNew * 8) + rNew));
-			}
-			rNew = r + 2;
-			cNew = c - 1;
-			if (rNew < 8 && cNew >= 0){
-				tmpBitBoard |= (1ULL << ((cNew * 8) + rNew));
-			}
-			rNew = r + 1;
-			cNew = c - 2;
-			if (rNew < 8 && cNew >= 0){
-				tmpBitBoard |= (1ULL << ((cNew * 8) + rNew));
-			}
-			rNew = r - 1;
-			cNew = c - 2;
-			if (rNew >= 0 && cNew >= 0){
-				tmpBitBoard |= (1ULL << ((cNew * 8) + rNew));
-			}
-			rNew = r - 2;
-			cNew = c - 1;
-			if (rNew >= 0 && cNew >= 0){
-				tmpBitBoard |= (1ULL << ((cNew * 8) + rNew));
-			}
-			rNew = r - 2;
-			cNew = c + 1;
-			if (rNew >= 0 && cNew < 8){
-				tmpBitBoard |= (1ULL << ((cNew * 8) + rNew));
-			}
-			rNew = r - 1;
-			cNew = c + 2;
-			if (rNew >= 0 && cNew < 8){
-				tmpBitBoard |= (1ULL << ((cNew * 8) + rNew));
-			}
-			KNIGHT_LOOKUP_TBL[(c * 8) + r] = tmpBitBoard;
-		}
-	}
-}
-
-void Board::INITIALIZE_RAYS(){
-	int pos;
-	for (int i = 0; i < 64; i++) {
-		//north
-		pos = i;
-		RAYS[i][0] = 0;
-		while(pos+8 < 63){
-			pos += 8;
-			RAYS[i][0] |= (1ULL << pos);
-		}
-		//ne
-		pos = i;
-		RAYS[i][1] = 0;
-		while((pos)%8 != 7 && pos+9 <= 63){
-			pos += 9;
-			RAYS[i][1] |= (1ULL << pos);
-		}
-		//e
-		pos = i;
-		RAYS[i][2] = 0;
-		while((pos)%8 != 7){
-			pos += 1;
-			RAYS[i][2] |= (1ULL << pos);
-		}
-		//se
-		pos = i;
-		RAYS[i][3] = 0;
-		while(pos-7 >= 0 && (pos-7)%8 != 0){
-			pos -= 7;
-			RAYS[i][3] |= (1ULL << pos);
-		}
-		//s
-		pos = i;
-		RAYS[i][4] = 0;
-		while((pos-8) >= 0){
-			pos -= 8;
-			RAYS[i][4] |= (1ULL << pos);
-		}
-		//sw
-		pos = i;
-		while(pos-9 >= 0 && (pos-9)%8 != 7){
-			pos -= 9;
-			RAYS[i][5] |= (1ULL << pos);
-		}
-		//w
-		pos = i;
-		while(pos%8 != 0){
-			pos -= 1;
-			RAYS[i][6] |= (1ULL << pos);
-		}
-		//nw
-		pos = i;
-		while((pos+7)%8 != 7 && (pos+7) < 63){
-			pos += 7;
-			RAYS[i][7] |= (1ULL << pos);
-		}
-	}
 }
