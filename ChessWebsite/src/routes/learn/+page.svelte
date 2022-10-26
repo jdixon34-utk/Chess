37dbf19982e99acf94e596bb888c626bd2fe347c
Jake <JakeShoffner27@gmail.com>
2022-10-26 17:09:30 -0400

    Fleshing out "How to Play" page

--- ChessWebsite/src/routes/learn/+page.svelte
+++ ChessWebsite/src/routes/learn/+page.svelte
@@ -1,6 +1,88 @@
 <script>
     import Navbar from "../../components/Navbar.svelte";
+    import Footer from "../../components/Footer.svelte";
 </script>
 
 <Navbar />
-<h1 class="pt-[9vh]">HOW TO PLAY PAGE</h1>
+
+<h1 class="pt-[12vh] flex justify-center items-center font-bold text-3xl bg-white">How to Play</h1>
+
+<div class="pt-[4vh] w-full h-full flex flex-col justify-center items-center px-[10vw] pb-[2vh]">
+    <p class="pb-[4vh]">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui distinctio 
+        voluptates rerum repudiandae laudantium ducimus ullam eos natus possimus. 
+        At cum doloribus saepe cumque voluptatum ducimus doloremque qui tempore 
+        explicabo! Eaque obcaecati, voluptatem voluptates porro beatae odit? Laborum 
+        accusantium voluptatibus hic provident nostrum repellat rerum dolorem. 
+        Consectetur placeat maxime reprehenderit vitae est sunt, magnam nulla eaque 
+        tenetur, voluptates non voluptatum soluta voluptatem corporis! Distinctio 
+        reiciendis tempora eaque architecto, voluptas perspiciatis! Vel veniam 
+        ratione nostrum possimus sapiente ad sunt neque libero voluptatem excepturi 
+        deleniti voluptas minus iure non, soluta nobis vero tempora minima est 
+        placeat similique! Cum voluptates officia eum! Natus esse et tempora, autem 
+        illo vel sapiente delectus impedit saepe suscipit reiciendis voluptatibus 
+        aperiam, eum est non. Omnis nobis corrupti sequi dolore quaerat! Hic enim 
+        quasi soluta consequuntur incidunt dolorem aliquam, iste, animi reprehenderit 
+        tenetur illo neque odit esse, molestias ratione laborum blanditiis eveniet 
+        cumque labore quis perferendis! Cum, iusto?
+    </p>
+
+    <p class="pb-[4vh]">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui distinctio 
+        voluptates rerum repudiandae laudantium ducimus ullam eos natus possimus. 
+        At cum doloribus saepe cumque voluptatum ducimus doloremque qui tempore 
+        explicabo! Eaque obcaecati, voluptatem voluptates porro beatae odit? Laborum 
+        accusantium voluptatibus hic provident nostrum repellat rerum dolorem. 
+        Consectetur placeat maxime reprehenderit vitae est sunt, magnam nulla eaque 
+        tenetur, voluptates non voluptatum soluta voluptatem corporis! Distinctio 
+        reiciendis tempora eaque architecto, voluptas perspiciatis! Vel veniam 
+        ratione nostrum possimus sapiente ad sunt neque libero voluptatem excepturi 
+        deleniti voluptas minus iure non, soluta nobis vero tempora minima est 
+        placeat similique! Cum voluptates officia eum! Natus esse et tempora, autem 
+        illo vel sapiente delectus impedit saepe suscipit reiciendis voluptatibus 
+        aperiam, eum est non. Omnis nobis corrupti sequi dolore quaerat! Hic enim 
+        quasi soluta consequuntur incidunt dolorem aliquam, iste, animi reprehenderit 
+        tenetur illo neque odit esse, molestias ratione laborum blanditiis eveniet 
+        cumque labore quis perferendis! Cum, iusto?
+    </p>
+
+    <p class="pb-[4vh]">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui distinctio 
+        voluptates rerum repudiandae laudantium ducimus ullam eos natus possimus. 
+        At cum doloribus saepe cumque voluptatum ducimus doloremque qui tempore 
+        explicabo! Eaque obcaecati, voluptatem voluptates porro beatae odit? Laborum 
+        accusantium voluptatibus hic provident nostrum repellat rerum dolorem. 
+        Consectetur placeat maxime reprehenderit vitae est sunt, magnam nulla eaque 
+        tenetur, voluptates non voluptatum soluta voluptatem corporis! Distinctio 
+        reiciendis tempora eaque architecto, voluptas perspiciatis! Vel veniam 
+        ratione nostrum possimus sapiente ad sunt neque libero voluptatem excepturi 
+        deleniti voluptas minus iure non, soluta nobis vero tempora minima est 
+        placeat similique! Cum voluptates officia eum! Natus esse et tempora, autem 
+        illo vel sapiente delectus impedit saepe suscipit reiciendis voluptatibus 
+        aperiam, eum est non. Omnis nobis corrupti sequi dolore quaerat! Hic enim 
+        quasi soluta consequuntur incidunt dolorem aliquam, iste, animi reprehenderit 
+        tenetur illo neque odit esse, molestias ratione laborum blanditiis eveniet 
+        cumque labore quis perferendis! Cum, iusto?
+    </p>
+
+    <p class="pb-[4vh]">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui distinctio 
+        voluptates rerum repudiandae laudantium ducimus ullam eos natus possimus. 
+        At cum doloribus saepe cumque voluptatum ducimus doloremque qui tempore 
+        explicabo! Eaque obcaecati, voluptatem voluptates porro beatae odit? Laborum 
+        accusantium voluptatibus hic provident nostrum repellat rerum dolorem. 
+        Consectetur placeat maxime reprehenderit vitae est sunt, magnam nulla eaque 
+        tenetur, voluptates non voluptatum soluta voluptatem corporis! Distinctio 
+        reiciendis tempora eaque architecto, voluptas perspiciatis! Vel veniam 
+        ratione nostrum possimus sapiente ad sunt neque libero voluptatem excepturi 
+        deleniti voluptas minus iure non, soluta nobis vero tempora minima est 
+        placeat similique! Cum voluptates officia eum! Natus esse et tempora, autem 
+        illo vel sapiente delectus impedit saepe suscipit reiciendis voluptatibus 
+        aperiam, eum est non. Omnis nobis corrupti sequi dolore quaerat! Hic enim 
+        quasi soluta consequuntur incidunt dolorem aliquam, iste, animi reprehenderit 
+        tenetur illo neque odit esse, molestias ratione laborum blanditiis eveniet 
+        cumque labore quis perferendis! Cum, iusto?
+    </p>
+</div>
+
+<Footer />
