ff72fff83986317757d6df8a8d5fcc49541dd177
jdixon34-cs340 <112104045+jdixon34-cs340@users.noreply.github.com>
2022-11-09 14:33:08 -0500

    updating to work with search function

--- src/game.cpp
+++ src/game.cpp
@@ -3,6 +3,7 @@
 #include "game.h"
 #include "board.h"
 #include "move.h"
+#include "search.h"
 //#include <emscripten/bind.h>
 //using namespace emscripten;
 //When using webassembly comment namespace out
@@ -10,23 +11,22 @@ using namespace std;
 
 
 std::string Game::playTurn(std::string FEN){
+    int eval;
     Board position;
-
-    position.initialize_tables();
-
     positions.push_back(position);
 
+    //set up position
+    position.initialize_tables();
     position.genBoardFromFEN(FEN);
 
     //position.printPosition();
+    //position.genMoves();
+    //position.printMoves();
 
-    //we have the position all set up
-    //now we find all possible moves
-    
-    position.genMoves();
-    position.printMoves();
- 
     //then do the search for the best move
+    eval = search(&position, 0);
+
+    printf("Evaluation: %d\n", eval);
 
     //return the updated FEN string
     return position.genFENFromBoard();
