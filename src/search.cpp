a5b00ffa69db3d8b3209d667274e08397f92f074
jdixon34-cs340 <112104045+jdixon34-cs340@users.noreply.github.com>
2022-11-10 00:00:01 -0500

    debugging

--- src/search.cpp
+++ src/search.cpp
@@ -1,46 +1,62 @@
+#include <cstring>
 #include "board.h"
+#include "move.h"
 #include "search.h"
 
 int search(Board* position, int depth){
-    if(depth == 2) return position->evaluatePosition();
+    if(depth == 4) return position->evaluatePosition();
 
     if(position->color == 0){
-        int current;
+        int wcurrent;
         int max = -10000;
         int capturedPieceType;
+        int numMovesWhite;
+        Move whiteMoves[128];
 
         position->genMoves();
-        for(int i = 0; i < position->moveIndex; i++){
-            capturedPieceType = position->makeMove(position->moves[i]);
+        memcpy(whiteMoves, position->moves, sizeof(Move) * 128);
+        numMovesWhite = position->moveIndex;
+        for(int i = 0; i < numMovesWhite; i++){
+            //printf("Depth %d Trying White Move %d: ", depth, i);
+            //whiteMoves[i].printMove();
+            capturedPieceType = position->makeMove(whiteMoves[i]);
             if(!position->squareUnderAttack(position->getKingPosition())){
                 position->color = 1;
-                current = search(position, depth++);
-                if(current > max){
-                    max = current;
-                    if(depth == 0) position->bestMove = position->moves[i];
+                wcurrent = search(position, depth + 1);
+                if(wcurrent > max){
+                    max = wcurrent;
+                    if(depth == 0) position->bestMove = whiteMoves[i];
                 }
+                position->color = 0;
             }
-            position->undoMove(position->moves[i], capturedPieceType);
+            position->undoMove(whiteMoves[i], capturedPieceType);
         }
         return max;
     }
     else{
-        int current;
+        int bcurrent;
         int min = 10000;
         int capturedPieceType;
+        int numMovesBlack;
+        Move blackMoves[128];
 
         position->genMoves();
-        for(int i = 0; i < position->moveIndex; i++){
-            capturedPieceType = position->makeMove(position->moves[i]);
+        memcpy(blackMoves, position->moves, sizeof(Move) * 128);
+        numMovesBlack = position->moveIndex;
+        for(int i = 0; i < numMovesBlack; i++){
+            //printf("Depth %d Trying Black Move %d: ", depth, i);
+            //blackMoves[i].printMove();
+            capturedPieceType = position->makeMove(blackMoves[i]);
             if(!position->squareUnderAttack(position->getKingPosition())){
                 position->color = 0;
-                current = search(position, depth++);
-                if(current < min){
-                    min = current;
-                    if(depth == 0) position->bestMove = position->moves[i];
+                bcurrent = search(position, depth + 1);
+                if(bcurrent < min){
+                    min = bcurrent;
+                    if(depth == 0) position->bestMove = blackMoves[i];
                 }
+                position->color = 1;
             }
-            position->undoMove(position->moves[i], capturedPieceType);
+            position->undoMove(blackMoves[i], capturedPieceType);
         }
         return min;
     }
