793e82c35f57ce16177672bc98d20bb4a0e054fe
Jake <JakeShoffner27@gmail.com>
2022-11-24 13:23:42 -0500

    Footer fix, alphabetical order

--- ChessWebsite/src/components/Footer.svelte
+++ ChessWebsite/src/components/Footer.svelte
@@ -1,9 +1,9 @@
 <div class="bg-black text-white h-[15vh] md:h-[11vh] w-full text-center flex justify-center items-center text-sm">
     <h1>
-        Fabricated by <a class="underline" href="https://github.com/Brandon-Marth" target="_blank">Brandon Marth</a>, 
-        <a class="underline" href="https://github.com/Dev5ter" target="_blank">Devin Munsey</a>, 
-        <a class="underline" href="https://github.com/jdixon34-cs340" target="_blank">Jared Dixon</a>, 
+        Fabricated by         <a class="underline" href="https://github.com/jdixon34-cs340" target="_blank">Jared Dixon</a>, 
+        <a class="underline" href="https://github.com/Brandon-Marth" target="_blank">Brandon Marth</a>, 
         <a class="underline" href="https://github.com/JMcknight75" target="_blank">Justin McKnight</a>,
+        <a class="underline" href="https://github.com/Dev5ter" target="_blank">Devin Munsey</a>, 
         and <a class="underline" href="https://github.com/Jxk0be" target="_blank">Jake Shoffner</a>.
     </h1>
 </div>
