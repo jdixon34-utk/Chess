bc5f4c2161d058481b52065391790d32aab3cb93
Jake <JakeShoffner27@gmail.com>
2022-11-29 13:32:31 -0500

    minor style update #3

--- ChessWebsite/src/routes/about/+page.svelte
+++ ChessWebsite/src/routes/about/+page.svelte
@@ -31,28 +31,29 @@
         <div class="flex flex-col">
             <h1 class="text-2xl font-semibold pb-[1vh]">Languages and Technologies</h1>
             <p class="pb-[2vh]">
-                We were able to use an array of different tools in our project. While the engine was done
-                solely in C++, we used a variation of technologies for the front end. Here they are listed.
+                We were able to use an array of different tools in our project. While the engine itself was done
+                solely in C++, we had to use a variety of different technologies on the front-end such as: Svelte, TailwindCSS, and Web Assembly.
+                Here is everything we used and what it was used for.
 
             <ul class="pb-[4vh]">
                 <li class="pb-[1vh]">
-                    <a class="text-[#c5986b] hover:text-[#b18960] font-bold underline" href="https://devdocs.io/cpp/" target="_blank">C++</a>
+                    <a class="text-[#de7864] hover:text-[#c8543d] font-bold underline" href="https://devdocs.io/cpp/" target="_blank">C++</a>
                     - Chess Engine
                 </li>
                 <li class="pb-[1vh]">
-                    <a class="text-[#c5986b] hover:text-[#b18960] font-bold underline" href="https://developer.mozilla.org/en-US/docs/WebAssembly" target="_blank">Web Assembly</a>
+                    <a class="text-[#de7864] hover:text-[#c8543d] font-bold underline" href="https://developer.mozilla.org/en-US/docs/WebAssembly" target="_blank">Web Assembly</a>
                     - Bridging Back End and Front End
                 </li>
                 <li class="pb-[1vh]">
-                    <a class="text-[#c5986b] hover:text-[#b18960] font-bold underline" href="https://svelte.dev/docs" target="_blank">Svelte</a>
+                    <a class="text-[#de7864] hover:text-[#c8543d] font-bold underline" href="https://svelte.dev/docs" target="_blank">Svelte</a>
                     - Web Application
                 </li>
                 <li class="pb-[1vh]">
-                    <a class="text-[#c5986b] hover:text-[#b18960] font-bold underline" href="https://v2.tailwindcss.com/docs" target="_blank">TailwindCSS</a>
+                    <a class="text-[#de7864] hover:text-[#c8543d] font-bold underline" href="https://v2.tailwindcss.com/docs" target="_blank">TailwindCSS</a>
                     - Major Styling
                 </li>
                 <li class="pb-[1vh]">
-                    <a class="text-[#c5986b] hover:text-[#b18960] font-bold underline" href="https://developer.mozilla.org/en-US/" target="_blank">JavaScript, HTML, CSS</a>
+                    <a class="text-[#de7864] hover:text-[#c8543d] font-bold underline" href="https://developer.mozilla.org/en-US/" target="_blank">JavaScript, HTML, CSS</a>
                     - Chess Board
             </ul>
         </div>
@@ -70,15 +71,15 @@
 
             <ul class="pb-[4vh]">
                 <li class="pb-[1vh]">
-                    <a class="text-[#c5986b] hover:text-[#b18960] font-bold underline" href="https://www.chesshouse.com/pages/chess-rules" target="_blank">The Rules of Chess</a>
+                    <a class="text-[#de7864] hover:text-[#c8543d] font-bold underline" href="https://www.chesshouse.com/pages/chess-rules" target="_blank">The Rules of Chess</a>
                     - A great website that goes into more detail on the basics above.
                 </li>
                 <li class="pb-[1vh]">
-                    <a class="text-[#c5986b] hover:text-[#b18960] font-bold underline" href="https://www.chess.com/learn-how-to-play-chess" target="_blank">7 Steps To Get You Started</a>
+                    <a class="text-[#de7864] hover:text-[#c8543d] font-bold underline" href="https://www.chess.com/learn-how-to-play-chess" target="_blank">7 Steps To Get You Started</a>
                     - Essential information with videos to help with concepts.
                 </li>
                 <li class="pb-[1vh]">
-                    <a class="text-[#c5986b] hover:text-[#b18960] font-bold underline" href="https://nextlevelchess.blog/beginner-chess-strategy/" target="_blank">Beginner Chess Strategy</a>
+                    <a class="text-[#de7864] hover:text-[#c8543d] font-bold underline" href="https://nextlevelchess.blog/beginner-chess-strategy/" target="_blank">Beginner Chess Strategy</a>
                     - After learning the basics, we recommend learning strategy.
                 </li>
             </ul>
@@ -86,7 +87,7 @@
 
         <!-- Contributors -->
         <div class="flex flex-col">
-            <h1 class="text-2xl font-semibold pb-[1vh]">Contributors</h1>
+            <h1 class="text-2xl font-semibold pb-[1vh]">Contributors and Repo</h1>
             <p class="pb-[2vh]">
                This project was created by the quintet listed below, with advice along the way from
                our TA 
@@ -96,24 +97,27 @@
 
             <ul class="pb-[4vh]">
                 <li class="pb-[1vh]">
-                    <a class="text-[#c5986b] hover:text-[#b18960] font-bold underline" href="https://github.com/jdixon34-cs340" target="_blank">Jared Dixon</a>
+                    <a class="text-[#de7864] hover:text-[#c8543d] font-bold underline" href="https://github.com/jdixon34-cs340" target="_blank">Jared Dixon</a>
                     - Back End (Chess Engine)
                 </li>
                 <li class="pb-[1vh]">
-                    <a class="text-[#c5986b] hover:text-[#b18960] font-bold underline" href="https://github.com/Brandon-Marth" target="_blank">Brandon Marth</a>
+                    <a class="text-[#de7864] hover:text-[#c8543d] font-bold underline" href="https://github.com/Brandon-Marth" target="_blank">Brandon Marth</a>
                     - Front End (Web Assembly and Web Application)
                 </li>
                 <li class="pb-[1vh]">
-                    <a class="text-[#c5986b] hover:text-[#b18960] font-bold underline" href="https://github.com/JMcknight75" target="_blank">Justin McKnight</a>
+                    <a class="text-[#de7864] hover:text-[#c8543d] font-bold underline" href="https://github.com/JMcknight75" target="_blank">Justin McKnight</a>
                     - Back End (Chess Engine)
                 </li>
                 <li class="pb-[1vh]">
-                    <a class="text-[#c5986b] hover:text-[#b18960] font-bold underline" href="https://github.com/Dev5ter" target="_blank">Devin Munsey</a>
+                    <a class="text-[#de7864] hover:text-[#c8543d] font-bold underline" href="https://github.com/Dev5ter" target="_blank">Devin Munsey</a>
                     - Back End (Chess Engine)
                 </li>
                 <li class="pb-[1vh]">
-                    <a class="text-[#c5986b] hover:text-[#b18960] font-bold underline" href="https://github.com/Jxk0be" target="_blank">Jake Shoffner</a>
+                    <a class="text-[#de7864] hover:text-[#c8543d] font-bold underline" href="https://github.com/Jxk0be" target="_blank">Jake Shoffner</a>
                     - Front End (Web Application)
+                <li class="pb-[1vh]">
+                     <a class="text-[#de7864] hover:text-[#c8543d] font-bold underline" href="https://github.com/utk-cs340-fall22/Chess" target="_blank">Project Repository</a>
+                    - Source Code (Front and Back End)
             </ul>
         </div>
 
