931f89e7cbac6ebc233677fb01a06aeab5a6bf71
Jake <JakeShoffner27@gmail.com>
2022-11-29 12:04:46 -0500

    minor styling update

--- ChessWebsite/src/components/Footer.svelte
+++ ChessWebsite/src/components/Footer.svelte
@@ -1,4 +1,4 @@
-<div class="bg-black text-white h-[15vh] md:h-[11vh] w-full text-center flex justify-center items-center text-sm">
+<div class="bg-[#161616] text-white h-[15vh] md:h-[11vh] w-full text-center flex justify-center items-center text-sm">
     <h1>
         Fabricated by         <a class="underline" href="https://github.com/jdixon34-cs340" target="_blank">Jared Dixon</a>, 
         <a class="underline" href="https://github.com/Brandon-Marth" target="_blank">Brandon Marth</a>, 
