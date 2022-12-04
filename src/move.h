4e36934f16214e02fa0aaf8ec3003ddec80bfc70
jdixon34-cs340 <112104045+jdixon34-cs340@users.noreply.github.com>
2022-12-04 00:29:28 -0500

    castling stuff

--- src/move.h
+++ src/move.h
@@ -8,8 +8,9 @@ public:
     char toSquare;
     char specialMove; //0 = none, 1 = en passant, 2 = castling, 3 = pawn promotion
     char promotedPiece; //0 = none, 1 = queen, 2 = rook, 3 = bishop, 4 = knight
+    int castleInfo[4];
 
-    void createMove(char fromSquareArg, char toSquareArg, char specialMoveArg, char promotedPieceArg);
+    void createMove(char fromSquareArg, char toSquareArg, char specialMoveArg, char promotedPieceArg, int whiteCastleRightsKS, int whiteCastleRightsQS, int blackCastleRightsKS, int blackCastleRightsQS);
     void printMove();
 };
 
