929fd0b4790823c2bcb62568b30878f00f7f6d44
jdixon34-cs340 <112104045+jdixon34-cs340@users.noreply.github.com>
2022-12-04 00:31:35 -0500

    endgame mode

--- src/game.cpp
+++ src/game.cpp
@@ -19,13 +19,13 @@ std::string Game::playTurn(std::string FEN){
     position.positionsEvaluated = 0;
     position.initialize_tables();
     position.genBoardFromFEN(FEN);
-
+    position.whiteCastled = 0;
+    position.blackCastled = 0;
 
     materialCount = position.getMaterialCount(0) + position.getMaterialCount(1);
     if(materialCount > 2000) {maxDepth = 4; position.isEndgame = 0;}
     else if(materialCount > 750) {maxDepth = 6; position.isEndgame = 1;}
     else {maxDepth = 8; position.isEndgame = 1;}
-
     //then do the search for the best move
     eval = search(&position, maxDepth, 0, -10010, 10010);
 
